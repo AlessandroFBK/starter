286e10e376d2018cc080d64b9ec4e34b0a553a9d
Alessandro Bacchiega <bacchiega@fbk.eu>
2024-08-22 11:56:51 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   constructor() {}
 
+  async ngOnInit(): Promise<void> {
+    const res = await cordova.plugins.health.isAvailable();
+    console.log('Health available? ', res);
+  }
 }
